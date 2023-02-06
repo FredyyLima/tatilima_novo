1c8bc3596126ab9f6f14b56a556172c8b0edc8ef
Frederico Lima <fredyy_lima@live.com>
2023-02-06 01:35:20 -0300

    .

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.set("view engine", "ejs");
 const path = require("path");
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, "assets")));
 
 app.get("/", (req, res) => {
     res.render("index");
